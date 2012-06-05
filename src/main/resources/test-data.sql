3c0b6801a3b8dc3bac7015224f2a8eb6e6009410
Amit Kumar <toamitkumar@gmail.com>
2012-06-05 16:11:42 -0400

    Added test sql file

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,10 @@
+create table Person (
+  id int primary key,
+  firstName varchar(20),
+  lastName varchar(20)
+);
+
+
+
+insert into Person values (1, 'Amit', 'Kumar');
+insert into Person values (2, 'Samik', 'Pal');
